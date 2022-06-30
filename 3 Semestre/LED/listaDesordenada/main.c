7735428be50420807668231674d45d9e99bb23b7
caiosobrinhoft <caio.sobrinho@unesp.br>
2022-06-29 22:36:15 -0300

    updating

---
+++ 3 Semestre/LED/listaDesordenada/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lista.h"
+
+int main(int argc, char *argv[])
+{
+  //Preencher aqui um exemplo prático que utilizar parte das operações da lista sequencial
+  
+  system("PAUSE");
+  return 0;
+}
+
